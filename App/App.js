3fa5f1ad568bdaecfdad15849034c24c1efcfa3b
DiteR <Personalizado_321@hotmail.com>
2022-04-18 22:04:05 -0400

    first commit

---
+++ App/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Button, Pressable} from 'react-native'
+
+function App() {
+    return (
+        <View>
+           <Text> Hola a todos </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        backgroundColor: "#fff",
+        padding: 20,
+        margin: 10,
+      },
+})
+
+export default App;
